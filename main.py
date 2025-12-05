eb1791db7a6ef1996ed64e109e905cb8e6d27e9e
Will Forbes <wforbes87@gmail.com>
2025-12-04 22:35:44 -0800

    CH3 L3:  project finished

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+import sys
 from stats import get_num_words, get_char_counts, sort_char_counts
 
-def get_book_text() -> str:
-    with open('books/frankenstein.txt', 'r') as f:
+def get_book_text(text: str) -> str:
+    with open(text, 'r') as f:
         file_contents = f.read()
     return file_contents
 
-def main() -> None:
-    book_text = get_book_text()
-    char_counts = get_char_counts(book_text)
+def print_report(text: str):
+    char_counts = get_char_counts(text)
     sorted_counts = sort_char_counts(char_counts)
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    get_num_words(book_text)
+    get_num_words(text)
     print("--------- Character Count -------")
     for item in sorted_counts:
         if item["char"].isalpha():
             print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
+
+
+def main() -> None:
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
+    print_report(book_text)
 
 main()
